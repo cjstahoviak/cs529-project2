13a69fb8f2db3344d9ac75110934c7879f01d66b
Calvin Stahoviak <cjstahoviak@gmail.com>
2024-03-27 19:25:44 -0600

    Created logistic_regression.py boiler-plate code for logistic regression. Needs to be checked for accuracy

---
+++ src/logistic_regression.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+
+
+class LogisticRegression(BaseEstimator, ClassifierMixin):
+    def __init__(self, learning_rate=0.01, max_iter=1000):
+        self.learning_rate = learning_rate
+        self.max_iter = max_iter
+
+    # def _sigmoid(self, z):
+    #     return 1 / (1 + np.exp(-z))
+
+    # def fit(self, X, y):
+    #     X, y = check_X_y(X, y)
+
+    #     # Initialize weights
+    #     self.coef_ = np.zeros(X.shape[1])
+    #     self.intercept_ = 0
+
+    #     # Gradient descent
+    #     for _ in range(self.max_iter):
+    #         linear_model = np.dot(X, self.coef_) + self.intercept_
+    #         y_pred = self._sigmoid(linear_model)
+    #         error = y_pred - y
+
+    #         # Update weights
+    #         self.coef_ -= self.learning_rate * np.dot(X.T, error) / X.shape[0]
+    #         self.intercept_ -= self.learning_rate * np.sum(error) / X.shape[0]
+
+    #     return self
+
+    # def predict_proba(self, X):
+    #     check_is_fitted(self)
+    #     X = check_array(X)
+
+    #     linear_model = np.dot(X, self.coef_) + self.intercept_
+    #     return self._sigmoid(linear_model)
+
+    # def predict(self, X):
+    #     probabilities = self.predict_proba(X)
+    #     return (probabilities >= 0.5).astype(int)
