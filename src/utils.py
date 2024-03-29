b8d1510516b42613fa774f36e408d736e3e43fe5
Nicholas Elmo Livingstone <nlivingsto@unm.edu>
2024-03-28 22:52:56 -0600

    Created describe as df func

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import librosa
 import pandas as pd
+from scipy.stats import describe
 
 
 def get_genre_from_path(path: Path):
@@ -31,3 +32,43 @@ def load_audio_to_df(data_dir: Path):
 
     df = pd.DataFrame.from_dict(audio_dict, orient="index")
     return df
+
+
+def describe_as_df(x, desc_kw_args={}):
+    """Convert the output of scipy.stats.describe to a DataFrame with MultiIndex columns.
+
+    Args:
+        x (_type_): Input data to describe.
+        desc_kw_args (dict, optional): Keword args to pass to describe function . Defaults to {}.
+
+    Returns:
+        DataFrame: DataFrame with MultiIndex columns.
+    """
+    stats = describe(x, **desc_kw_args)
+    data = {}
+
+    for field in stats._fields:
+        values = getattr(stats, field)
+
+        # Special handling for 'minmax'
+        if field == "minmax":
+            tmp_fields = ["min", "max"]
+            tmp_vals = [values[0], values[1]]
+        else:
+            tmp_fields = [field]
+            tmp_vals = [values]
+
+        for f, v in zip(tmp_fields, tmp_vals):
+            # Values that are arrays
+            if hasattr(v, "__len__") and not isinstance(v, (str, bytes)):
+                for i, value in enumerate(v, 1):  # Start index from 1
+                    data[(f, i)] = [value]
+            # Handle scalar fields
+            else:
+                data[(f, "")] = [v]
+
+    # Create a DataFrame with MultiIndex columns from the dictionary
+    stats_df = pd.DataFrame(data)
+    stats_df.columns.names = ["stat", ""]
+
+    return stats_df
