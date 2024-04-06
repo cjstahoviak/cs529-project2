fdd3771348700d72fcc63f464d042874d86b0ce0
Nicholas Livingstone <nicholaselmolivingstone@gmail.com>
2024-04-05 18:49:58 -0600

    Improved data preprocess with column transformer

--- experiments/data_preprocess.py
+++ experiments/data_preprocess.py
@@ -1,26 +1,33 @@
+from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
-from sklearn.pipeline import FeatureUnion, Pipeline
+from joblib import parallel_config
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 from custom_transformers import ElementwiseSummaryStats, LibrosaTransformer
 
-# Load data
+parallel_config(n_jobs=-2)
+
+# Window sizes to generate features for
+WIN_SIZES = [8192]
+
+# File Paths
 train_fpath = Path("../data/processed/train_data.pkl").resolve()
 test_fpath = Path("../data/processed/test_data.pkl").resolve()
+dest_dir = Path("../data/processed/feature_extracted").resolve()
 
+# Load data
 train_df = pd.read_pickle(train_fpath)
 test_df = pd.read_pickle(test_fpath)
 
 y_train = train_df["target"]
-X_train = train_df["audio"]
+# Access with loc so it returns a DataFrame
+X_train = train_df.loc[:, ["audio"]]
+X_test = test_df.loc[:, ["audio"]]
 
-X_test = test_df["audio"]
-
-WIN_SIZES = [1024, 2048, 4096]
-
-librosa_feature_pipelines = []
-
+# Librosa features to extract
 librosa_features = [
     "mfcc",
     "chroma_stft",
@@ -29,46 +36,57 @@ librosa_features = [
     "spectral_rolloff",
     "spectral_contrast",
     "zero_crossing_rate",
+    "tonnetz",
+    "tempogram_ratio",
 ]
 
+pipe = Pipeline(
+    [
+        (
+            "librosa_features",
+            ColumnTransformer(
+                [
+                    (feature, LibrosaTransformer(feature=feature), ["audio"])
+                    for feature in librosa_features
+                ]
+            ),
+        ),
+        ("stats", ElementwiseSummaryStats()),
+    ]
+)
 
-for feature in librosa_features:
-
-    new_pipeline = Pipeline(
-        [
-            ("transformer", LibrosaTransformer(feature=feature)),
-            ("stats", ElementwiseSummaryStats(desc_kw_args={"axis": 1})),
-        ]
-    )
-
-    if feature == "mfcc":
-        new_pipeline.set_params(transformer__n_mfcc=20)
-
-    librosa_feature_pipelines.append((feature, new_pipeline))
-
-feature_extractor = FeatureUnion(transformer_list=librosa_feature_pipelines)
+pipe.set_output(transform="pandas")
 
 for win_size in WIN_SIZES:
+    print("Extracting features for window size:", win_size)
 
-    train_dict = {}
-    test_dict = {}
+    params = {
+        "librosa_features__mfcc__n_mfcc": 20,
+        "librosa_features__mfcc__n_fft": win_size,
+        "librosa_features__chroma_stft__n_fft": win_size,
+        "librosa_features__spectral_centroid__n_fft": win_size,
+        "librosa_features__spectral_bandwidth__n_fft": win_size,
+        "librosa_features__spectral_rolloff__n_fft": win_size,
+        "librosa_features__spectral_contrast__n_fft": win_size,
+        "librosa_features__zero_crossing_rate__frame_length": win_size,
+    }
 
-    for feature, pipeline in feature_extractor.named_transformers.items():
-        param_name = "frame_length" if feature == "zero_crossing_rate" else "n_fft"
-        pipeline.set_params(**{f"transformer__{param_name}": win_size})
+    pipe.set_params(**params)
 
-        # Fit and transform the train/test data
-        train_dict[feature] = pipeline.fit_transform(X_train)
-        test_dict[feature] = pipeline.transform(X_test)
+    print("Transforming train data...")
+    time_start = datetime.now()
+    train_df = pipe.fit_transform(X_train)
+    time_end = datetime.now()
+    print(f"Completed in: {time_end - time_start}")
 
-    train_df = pd.concat(train_dict, axis=1, names=["feature", "statistic", "channel"])
-    train_df[("target", "", "")] = y_train
+    print("Transforming test data...")
+    time_start = datetime.now()
+    test_df = pipe.transform(X_test)
+    time_end = datetime.now()
+    print(f"Completed in: {time_end - time_start}")
 
-    test_df = pd.concat(test_dict, axis=1, names=["feature", "statistic", "channel"])
-
-    pd.DataFrame(train_df).to_csv(
-        f"../data/processed/feature_extracted/train_features_{win_size}.csv"
-    )
-    pd.DataFrame(test_df).to_csv(
-        f"../data/processed/feature_extracted/test_features_{win_size}.csv"
-    )
+    print("Saving features...")
+    train_df["target"] = y_train
+    pd.DataFrame(train_df).to_pickle(dest_dir / f"train_features_{win_size}.pkl")
+    pd.DataFrame(test_df).to_pickle(dest_dir / f"test_features_{win_size}.pkl")
+    print("")
