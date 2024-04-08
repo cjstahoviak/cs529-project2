a939f95d4378f5519866a4d36a35991ff1b1c33e
Nicholas Livingstone <nicholaselmolivingstone@gmail.com>
2024-04-08 02:34:01 -0600

    Modified model_comparison to utilize gridsearchcv and mlflow

--- experiments/model_comparison.py
+++ experiments/model_comparison.py
@@ -1,51 +1,161 @@
+import os
+import tempfile
+from datetime import datetime
 from pathlib import Path
 
+import mlflow
 import pandas as pd
-import numpy as np
-
 from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.pipeline import Pipeline
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
 from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
+from custom_transformers import WindowSelector
 from logistic_regression import SoftmaxRegression
 
-train_features_fpath = Path(
-    "../data/processed/feature_extracted/train_features_1024.csv"
-).resolve()
-train_features_df = pd.read_csv(train_features_fpath, index_col=0)
-X_train = train_features_df.drop(["target"], axis=1)
-X_train.columns = X_train.columns + "_" + X_train.iloc[0] + "_" + X_train.iloc[1]
-X_train = X_train.iloc[2:]
-X_train = X_train.apply(pd.to_numeric, errors="coerce")
+N_JOBS = os.cpu_count() * 2
+EXPERIMENT_NAME = "/cs529_project_2_softmax_gridsearch"
+DATA_FOLDER_PATH = Path("../data/processed/feature_extracted/pickle").resolve()
+WIN_SIZES = [1024, 2048, 4096, 8192]
 
-y_train = train_features_df["target"].iloc[2:]
-X_test = X_train
-y_test = y_train
 
-print(X_train.head())
-print("Total features in X_train: " + str(X_train.shape[1]))
-print("Total instances in X_train: " + str(X_train.shape[0]))
+def load_data():
+    train_dict = {}
+    test_dict = {}
 
-# Defining pipelines for each model
-pipelines = {
-    "Softmax Regression": make_pipeline(StandardScaler(), PCA(n_components=0.99), SoftmaxRegression(
-                learning_rate=0.01,
-                learning_rate_decay=0.0,
-                max_iter=100_000,
-                weight_defaults="zero",
-                temperature=1.0,
-            ),),
-    "Random Forest": make_pipeline(StandardScaler(), PCA(n_components=0.95), RandomForestClassifier(random_state=42)),
-    "Gaussian Naive Bayes": make_pipeline(StandardScaler(), PCA(n_components=0.95), GaussianNB()),
-    "Gradient Boosting Machines": make_pipeline(StandardScaler(), PCA(n_components=0.95), GradientBoostingClassifier(random_state=42)),
-    "SVM": make_pipeline(StandardScaler(), PCA(n_components=0.95), SVC(random_state=42))
-}
+    y_train = None
 
-for name, model in pipelines.items():
-    cv_scores = cross_val_score(model, X_train, y_train, cv=5, scoring='accuracy')
-    print(f"{name} CV accuracy: {np.mean(cv_scores):.3f}")
+    for win_size in WIN_SIZES:
+        train_df: pd.DataFrame = pd.read_pickle(
+            DATA_FOLDER_PATH / f"train_features_{win_size}.pkl"
+        )
+        X_test: pd.DataFrame = pd.read_pickle(
+            DATA_FOLDER_PATH / f"test_features_{win_size}.pkl"
+        )
+
+        X_train = train_df.drop(columns=["target"], level=0)
+
+        train_dict[win_size] = X_train
+        test_dict[win_size] = X_test
+
+        if y_train is None:
+            y_train = train_df["target"].values.ravel()
+
+    X_train = pd.concat(
+        train_dict.values(),
+        axis=1,
+        keys=WIN_SIZES,
+        names=["win_size", "feature", "stat"],
+    )
+    X_test = pd.concat(
+        test_dict.values(),
+        axis=1,
+        keys=WIN_SIZES,
+        names=["win_size", "feature", "stat"],
+    )
+
+    return X_train, y_train, X_test
+
+
+def main():
+    # MLflow setup
+    mlflow.set_tracking_uri("databricks")
+    mlflow.set_experiment(EXPERIMENT_NAME)
+    mlflow.autolog(log_datasets=False, log_models=False)
+
+    # Load data
+    print("Loading data...")
+    X_train, y_train, X_test = load_data()
+
+    print("Data loaded:")
+    print(f"X_train:\n{X_train.head()}")
+    print(f"y_train:\n{y_train}")
+    print(f"X_test:\n{X_test.head()}")
+
+    # Define pipeline
+    print("Creating pipeline...")
+    pipe = Pipeline(
+        [
+            ("win_selector", WindowSelector()),
+            ("scaler", StandardScaler()),
+            ("pca", PCA()),
+            ("classifier", None),
+        ]
+    )
+
+    # Create parameter grids to search over
+    base_param_grid = {
+        "win_selector__win_size": WIN_SIZES + ["All"],
+        "pca__n_components": [0.80, 0.9, 0.99],
+    }
+
+    classifier_grids = [
+        {
+            "classifier": [SoftmaxRegression()],
+            "classifier__regularization": ["l2"],
+            "classifier__lam": [0.01, 0.1, 0.05, 1],
+            "classifier__tol": [1e-4, 1e-8],
+        },
+        {
+            "classifier": [RandomForestClassifier()],
+            "classifier__n_estimators": [100, 200, 300],
+            "classifier__max_depth": [5, 10, 15],
+        },
+        {
+            "classifier": [GaussianNB()],
+        },
+        {
+            "classifier": [GradientBoostingClassifier()],
+            "classifier__n_estimators": [100, 200, 300],
+            "classifier__loss": ["log_loss", "deviance", "exponential"],
+            "classifier__learning_rate": [0.01, 0.1, 0.5],
+        },
+        {
+            "classifier": [SVC()],
+            "classifier__C": [0.1, 1, 10],
+            "classifier__kernel": ["linear", "poly", "rbf"],
+        },
+    ]
+
+    param_grids = [
+        {**base_param_grid, **classifier_grid} for classifier_grid in classifier_grids
+    ]
+
+    # Run grid search
+    N_SPLITS = 5
+
+    gc = GridSearchCV(
+        pipe,
+        param_grid=param_grids,
+        n_jobs=N_JOBS,
+        cv=StratifiedShuffleSplit(n_splits=N_SPLITS, random_state=42),
+        verbose=2,
+        pre_dispatch="n_jobs",  # Reduce memory consumption
+    )
+
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    with mlflow.start_run(run_name=f"softmax_gridsearch_{timestamp}"):
+
+        # Fit the model
+        gc.fit(X_train, y_train)
+
+        # Run best model on kaggle test data
+        y_pred = gc.best_estimator_.predict(X_test)
+        test_results = pd.DataFrame({"class": y_pred}, index=X_test.index)
+        test_results.index.name = "id"
+
+        # Save kaggle test results
+        with tempfile.TemporaryDirectory() as tmpdir:
+            kaggle_submission_fname = (
+                Path(tmpdir) / f"kaggle_submission_{timestamp}.csv"
+            )
+            test_results.to_csv(kaggle_submission_fname)
+            mlflow.log_artifact(kaggle_submission_fname)
+
+
+if __name__ == "__main__":
+    main()
