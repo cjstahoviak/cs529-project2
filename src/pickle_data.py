9d77aa002f5c190b13d506420f9fc68402c92aff
Nicholas Livingstone <nicholaselmolivingstone@gmail.com>
2024-04-11 01:39:32 -0600

    Updated pickle_data.py

--- src/pickle_data.py
+++ src/pickle_data.py
@@ -3,33 +3,42 @@ from pathlib import Path
 
 from utils import load_audio_to_df
 
-parser = ArgumentParser(
-    prog="Pickles Audio Data",
-    description="Converts raw audio data to pickled pandas DataFrames. For each subdirectory in the source directory, a pickled DataFrame is created and saved in the destination directory.",
-)
 
-parser.add_argument(
-    "--source",
-    "-s",
-    type=Path,
-    default="../data/raw/",
-    help="Path to raw audio data. Should be a directory containing subdirectories of audio files.",
-)
-parser.add_argument(
-    "--dest",
-    "-d",
-    type=Path,
-    default="../data/processed/",
-    help="Path to save pickled data.",
-)
+def main():
+    parser = ArgumentParser(
+        prog="Pickles Audio Data",
+        description="Converts raw audio data to a pickled pandas DataFrame. Will search for .au files in the given directory.",
+    )
 
-args = parser.parse_args()
-source_dir: Path = args.source
-dest_dir: Path = args.dest
+    parser.add_argument(
+        "--source",
+        "-s",
+        type=Path,
+        default="../data/raw/test/",
+        help="Path to folder containing audio data.",
+    )
+    parser.add_argument(
+        "--dest",
+        "-d",
+        type=Path,
+        default="../data/processed/",
+        help="Path to save pickled data. Should be a directory.",
+    )
 
-# Process raw audio data and save as pickled files.
-for dir in source_dir.glob("*/"):
-    dir_name = dir.parts[-1]
-    df = load_audio_to_df(dir)
+    args = parser.parse_args()
+    source_dir: Path = args.source
+    dest_dir: Path = args.dest
+
+    # Process raw audio data and save as pickled files.
+    dir_name = source_dir.stem
+    print("Pickling data from:", source_dir)
+    df = load_audio_to_df(source_dir)
     dest = dest_dir / f"{dir_name}.pkl"
+    print(f"Collected {len(df)} audio files.")
+    print("Saving pickled data to:", dest)
     df.to_pickle(dest)
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
