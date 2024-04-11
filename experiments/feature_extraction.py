1f205faf6654f7f62d97c71656d8623ca2f03cd1
Nicholas Livingstone <nicholaselmolivingstone@gmail.com>
2024-04-11 01:41:36 -0600

    Placed feature extraction into a main func

--- experiments/feature_extraction.py
+++ experiments/feature_extraction.py
@@ -10,89 +10,95 @@ from src.custom_transformers import ElementwiseSummaryStats, LibrosaTransformer
 
 N_JOBS = 1
 
-parallel_config(n_jobs=N_JOBS)
 
-# Window sizes to generate features for
-WIN_SIZES = [512, 1024, 2048, 4096, 8192]
+def main():
+    parallel_config(n_jobs=N_JOBS)
 
-# File Paths
-train_fpath = Path("../data/processed/train_data.pkl").resolve()
-test_fpath = Path("../data/processed/test_data.pkl").resolve()
-dest_dir = Path("../data/processed/feature_extracted").resolve()
+    # Window sizes to generate features for
+    WIN_SIZES = [512, 1024, 2048, 4096, 8192]
 
-if not dest_dir.exists():
-    dest_dir.mkdir(parents=True)
+    # File Paths
+    train_fpath = Path("../data/processed/train_data.pkl").resolve()
+    test_fpath = Path("../data/processed/test_data.pkl").resolve()
+    dest_dir = Path("../data/processed/feature_extracted").resolve()
 
-# Load data
-train_df = pd.read_pickle(train_fpath)
-test_df = pd.read_pickle(test_fpath)
+    if not dest_dir.exists():
+        dest_dir.mkdir(parents=True)
 
-y_train = train_df["target"]
-# Access with loc so it returns a DataFrame
-X_train = train_df.loc[:, ["audio"]]
-X_test = test_df.loc[:, ["audio"]]
+    # Load data
+    train_df = pd.read_pickle(train_fpath)
+    test_df = pd.read_pickle(test_fpath)
 
-# Librosa features to extract
-librosa_features = [
-    "mfcc",
-    "chroma_stft",
-    "chroma_cens",
-    "spectral_centroid",
-    "spectral_bandwidth",
-    "spectral_rolloff",
-    "spectral_contrast",
-    "zero_crossing_rate",
-    "tonnetz",
-    "tempogram_ratio",
-]
+    y_train = train_df["target"]
+    # Access with loc so it returns a DataFrame
+    X_train = train_df.loc[:, ["audio"]]
+    X_test = test_df.loc[:, ["audio"]]
 
-pipe = Pipeline(
-    [
-        (
-            "librosa_features",
-            ColumnTransformer(
-                [
-                    (feature, LibrosaTransformer(feature=feature), ["audio"])
-                    for feature in librosa_features
-                ]
-            ),
-        ),
-        ("stats", ElementwiseSummaryStats()),
+    # Librosa features to extract
+    librosa_features = [
+        "mfcc",
+        "chroma_stft",
+        "chroma_cens",
+        "spectral_centroid",
+        "spectral_bandwidth",
+        "spectral_rolloff",
+        "spectral_contrast",
+        "zero_crossing_rate",
+        "tonnetz",
+        "tempogram_ratio",
     ]
-)
 
-pipe.set_output(transform="pandas")
+    pipe = Pipeline(
+        [
+            (
+                "librosa_features",
+                ColumnTransformer(
+                    [
+                        (feature, LibrosaTransformer(feature=feature), ["audio"])
+                        for feature in librosa_features
+                    ]
+                ),
+            ),
+            ("stats", ElementwiseSummaryStats()),
+        ]
+    )
 
-for win_size in WIN_SIZES:
-    print("Extracting features for window size:", win_size)
+    pipe.set_output(transform="pandas")
 
-    params = {
-        "librosa_features__mfcc__n_mfcc": 20,
-        "librosa_features__mfcc__n_fft": win_size,
-        "librosa_features__chroma_stft__n_fft": win_size,
-        "librosa_features__spectral_centroid__n_fft": win_size,
-        "librosa_features__spectral_bandwidth__n_fft": win_size,
-        "librosa_features__spectral_rolloff__n_fft": win_size,
-        "librosa_features__spectral_contrast__n_fft": win_size,
-        "librosa_features__zero_crossing_rate__frame_length": win_size,
-    }
+    for win_size in WIN_SIZES:
+        print("Extracting features for window size:", win_size)
 
-    pipe.set_params(**params)
+        params = {
+            "librosa_features__mfcc__n_mfcc": 20,
+            "librosa_features__mfcc__n_fft": win_size,
+            "librosa_features__chroma_stft__n_fft": win_size,
+            "librosa_features__spectral_centroid__n_fft": win_size,
+            "librosa_features__spectral_bandwidth__n_fft": win_size,
+            "librosa_features__spectral_rolloff__n_fft": win_size,
+            "librosa_features__spectral_contrast__n_fft": win_size,
+            "librosa_features__zero_crossing_rate__frame_length": win_size,
+        }
 
-    print("Transforming train data...")
-    time_start = datetime.now()
-    train_df = pipe.fit_transform(X_train)
-    time_end = datetime.now()
-    print(f"Completed in: {time_end - time_start}")
+        pipe.set_params(**params)
 
-    print("Transforming test data...")
-    time_start = datetime.now()
-    test_df = pipe.transform(X_test)
-    time_end = datetime.now()
-    print(f"Completed in: {time_end - time_start}")
+        print("Transforming train data...")
+        time_start = datetime.now()
+        train_df = pipe.fit_transform(X_train)
+        time_end = datetime.now()
+        print(f"Completed in: {time_end - time_start}")
 
-    print("Saving features...")
-    train_df["target"] = y_train
-    pd.DataFrame(train_df).to_pickle(dest_dir / f"train_features_{win_size}.pkl")
-    pd.DataFrame(test_df).to_pickle(dest_dir / f"test_features_{win_size}.pkl")
-    print("")
+        print("Transforming test data...")
+        time_start = datetime.now()
+        test_df = pipe.transform(X_test)
+        time_end = datetime.now()
+        print(f"Completed in: {time_end - time_start}")
+
+        print("Saving features...")
+        train_df["target"] = y_train
+        pd.DataFrame(train_df).to_pickle(dest_dir / f"train_features_{win_size}.pkl")
+        pd.DataFrame(test_df).to_pickle(dest_dir / f"test_features_{win_size}.pkl")
+        print("")
+
+
+if __name__ == "__main__":
+    main()
